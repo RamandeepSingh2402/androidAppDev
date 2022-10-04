8f5bb976a5d6c45859a69075a8cf62cdb986a3e4
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-10-04 17:40:11 -0300

    created a basic app which takes name and shows the same on button click

--- FirstApp/app/src/main/java/com/example/helloworld/MainActivity.java
+++ FirstApp/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,14 +3,24 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //TODO: complete this
+    }
+
+    // onClick method
+    public void onBtnClick(View view){
+        //changing the text displayed in the textView
+        TextView txtMessage = findViewById(R.id.txtMessage);
+        EditText editTxtName = findViewById(R.id.editTxtName);
+        txtMessage.setText("Welcome to the app " + editTxtName.getText().toString());
     }
 }
